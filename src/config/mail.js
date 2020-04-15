1b85d6861688eae77c526c19d4fe5b419acac2e2
Joao Sousa <joao.rsousa23@gmail.com>
2020-04-16 00:14:18 +0930

    first gostack commit

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '0e0125c612ca09',
+    pass: 'e4aa7d2c851704',
+  },
+  default: {
+    from: 'Team gobarber <norely@gobarber.com>',
+  },
+};
---
+++ src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+  dsn:
+    'https://7a2685db1aa94e8f971d42cf09f7d6ff@o378268.ingest.sentry.io/5201421',
+};
